319e6245d0ecd478ddefd2c014d8a088207add45
Abhishek Chakraborty <deadbeatfour@gmail.com>
2017-06-19 21:42:42 +0530

    removed mobile unslick

--- static/js/dev/gallery.js
+++ static/js/dev/gallery.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-
-	if ($('.nav').css('flex-direction')=='column'){
-		$('.gallery-container-main').slick('unslick');
-	
-
-	} else if ($('.nav').css('flex-direction')=='row'){
 		$('.gallery-container-main').slick(
 			{
 				arrows: false,
@@ -16,29 +10,5 @@ $(document).ready(function(){
 				slidesToScroll: 1,
 				
 			});
-
-	}
-
-});
-$(window).on('resize', function(){
-
-	if ($('.nav').css('flex-direction')=='column'){
-		$('.gallery-container-main').slick('unslick');
-	
-
-	} else if ($('.nav').css('flex-direction')=='row'){
-		$('.gallery-container-main').slick(
-			{
-				arrows: false,
-				autoplay: true,
-				speed: 500,
-				fade: true,
-				Infinite: true,
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				
-			});
-
-	}
 
 });
--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-
-	if ($('.nav').css('flex-direction')=='column'){
-		$('.gallery-container-main').slick('unslick');
-	
-
-	} else if ($('.nav').css('flex-direction')=='row'){
 		$('.gallery-container-main').slick(
 			{
 				arrows: false,
@@ -17,31 +11,8 @@ $(document).ready(function(){
 				
 			});
 
-	}
-
 });
-$(window).on('resize', function(){
 
-	if ($('.nav').css('flex-direction')=='column'){
-		$('.gallery-container-main').slick('unslick');
-	
-
-	} else if ($('.nav').css('flex-direction')=='row'){
-		$('.gallery-container-main').slick(
-			{
-				arrows: false,
-				autoplay: true,
-				speed: 500,
-				fade: true,
-				Infinite: true,
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				
-			});
-
-	}
-
-});
 // Toggle the menu
 $(document).ready(function(){
 	if ($('.nav').css('flex-direction')=='column'){
