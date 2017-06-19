130c23d348114f58f8ff84452d788d4680b405d3
Abhishek Chakraborty <deadbeatfour@gmail.com>
2017-06-19 10:58:00 +0530

    Navbar update

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,48 @@
+// Toggle the menu
+$(document).ready(function(){
+	if ($('.nav').css('flex-direction')=='column'){
+			$('#menu-toggle-button-close').hide();
+			$('.nav').hide();
+
+	} else if ($('.nav').css('flex-direction')=='row'){
+			$('.nav').show();
+
+	}
+})
+
+$(window).on('resize',function(){
+	if ($('.nav').css('flex-direction')=='row'){
+			$('.nav').show();
+
+	}
+	
+});
+$('#menu-toggle-button').on('click',function(){
+		// $(this).toggleClass( "fa-bars" );
+		$('#menu-toggle-button').hide();
+		$('#menu-toggle-button-close').show();
+
+		$('.nav').slideDown({
+			duration: 300,
+			easing: 'swing'
+		});
+
+});
+
+$('#menu-toggle-button-close').on('click',function(){
+		// $(this).toggleClass( "fa-bars" );
+		$('#menu-toggle-button').show();
+		$('#menu-toggle-button-close').hide();
+
+		$('.nav').slideUp({
+			duration: 300,
+			easing: 'swing'
+		});
+
+});
+
+
+
 $(document).ready(function(){
 	$('.testimonials').slick(
 	{
