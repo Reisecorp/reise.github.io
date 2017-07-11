372c52692027bce80e1d3cdfc0f7e6313888f368
Abhishek Chakraborty <deadbeatfour@gmail.com>
2017-07-11 13:17:01 +0530

    made services slider

--- static/js/main.js
+++ static/js/main.js
@@ -59,6 +59,17 @@ $(document).ready(function(){
 			slidesToScroll: 1,
 			
 		});
+	$('.services-table').slick(
+		{
+			arrows: false,
+			autoplay: true,
+			speed: 500,
+			fade: true,
+			Infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			
+		});	
 	$('.why-us-content-title').slick(
 		{
 			arrows: false,
